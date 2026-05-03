f3a2de02bea6e0fe46a3a4659c526e84e0e849fb
priyadharshini-15mp <priyadharshinipriya5680@gmail.com>
2026-05-03 16:43:55 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use("/api/post", postRouter);
 
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log("Server is running on port ", PORT);
